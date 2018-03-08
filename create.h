c48ba9bf8b6a6b47ef4c55482fb1524edbb31d62
Barny90 <36065497+Barny90@users.noreply.github.com>
2018-03-07 23:25:13 -0800

    Create create.h

---
+++ create.h
@@ -0,0 +1,19 @@
+#ifndef create_hpp
+#define create_hpp
+
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+
+using namespace std;
+
+void create(SDL_Surface *pic, SDL_Renderer *renderer1, SDL_Window *win, SDL_Event windowEvent1);
+void create1(SDL_Surface *pic1, SDL_Renderer *renderer1, SDL_Window *win, SDL_Event windowEvent1);
+void create2(SDL_Surface *pic2, SDL_Renderer *renderer1, SDL_Window *win, SDL_Event windowEvent1);
+void create3(SDL_Surface *pic3, SDL_Renderer *renderer1, SDL_Window *win, SDL_Event windowEvent1);
+void create4(SDL_Surface *pic4, SDL_Renderer *renderer1, SDL_Window *win, SDL_Event windowEvent1);
+
+#endif /* create_hpp */
