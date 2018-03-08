c3706db0585da00e453535f8a40e26e9fc9512eb
Barny90 <36065497+Barny90@users.noreply.github.com>
2018-03-07 23:26:11 -0800

    Create choose.h

---
+++ choose.h
@@ -0,0 +1,19 @@
+#ifndef choose_hpp
+#define choose_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+
+void choose(SDL_Surface *pic, SDL_Surface *pic2, SDL_Surface *pic3, SDL_Surface *pic4, SDL_Surface *pic5, SDL_Surface *pic6, SDL_Renderer *renderer1, SDL_Window *win, SDL_Event windowEvent1);
+
+void choose1(SDL_Surface *pic, SDL_Surface *pic2, SDL_Surface *pic3, SDL_Surface *pic4, SDL_Surface *pic5, SDL_Surface *pic6, SDL_Renderer *renderer1, SDL_Window *win, SDL_Event windowEvent1);
+
+void choose2(SDL_Surface *pic, SDL_Surface *pic2, SDL_Surface *pic3, SDL_Surface *pic4, SDL_Surface *pic5, SDL_Surface *pic6, SDL_Renderer *renderer1, SDL_Window *win, SDL_Event windowEvent1);
+
+void choose3(SDL_Surface *pic, SDL_Surface *pic2, SDL_Surface *pic3, SDL_Surface *pic4, SDL_Surface *pic5, SDL_Surface *pic6, SDL_Renderer *renderer1, SDL_Window *win, SDL_Event windowEvent1);
+
+void choose4(SDL_Surface *pic, SDL_Surface *pic2, SDL_Surface *pic3, SDL_Surface *pic4, SDL_Surface *pic5, SDL_Surface *pic6, SDL_Renderer *renderer1, SDL_Window *win, SDL_Event windowEvent1);
+
+#endif /* choose_hpp */
